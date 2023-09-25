2213b3f9a60e3d64c02d3039bf3ad6cb5b276fde
natan210 <nwisrael@wisc.edu>
2023-09-25 13:33:06 -0500

    first draft

---
+++ src/main/java/com/cooksys/Hashtag.java
@@ -0,0 +1,30 @@
+package com.cooksys;
+
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Hashtag {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+    private String label;
+
+    private Timestamp firstUsed;
+
+    private Timestamp lastUsed;
+
+
+
+}
