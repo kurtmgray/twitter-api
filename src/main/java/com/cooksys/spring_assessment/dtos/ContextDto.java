8e2dd649d69410f2494772d08a522eccc4cb224b
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-09-27 10:08:03 -0500

    Updated ContextDto.java

--- src/main/java/com/cooksys/spring_assessment/dtos/ContextDto.java
+++ src/main/java/com/cooksys/spring_assessment/dtos/ContextDto.java
@@ -11,65 +11,71 @@ import java.util.Comparator;
 
 import lombok.Getter;
 
-@Getter
+@NoArgsConstructor
+@Data
 public class ContextDto {
+
+	private TweetResponseDto target;
+	private List<TweetResponseDto> before;
+	private List<TweetResponseDto> after;
 	
-	private static TweetRepository tweetRepository;
 	
-	public ContextDto(Tweet _target, TweetRepository _tweetRepository) {
-		tweetRepository = _tweetRepository;
-		
-		Comparator<Tweet> postedDateSorter = (t1, t2) -> t1.getPosted().compareTo(t2.getPosted());
-		
-		target = _target;
-		before = createBefore(target, new TreeSet<Tweet>(postedDateSorter));
-		after = createAfter(target, new TreeSet<Tweet>(postedDateSorter));
-	}
+	// private static TweetRepository tweetRepository;
 	
-	public ContextDto(Tweet _target) throws NotFoundException {
-		if (tweetRepository == null) {
-			throw new NotFoundException("No tweet repository found.");
-		}
+	// public ContextDto(Tweet _target, TweetRepository _tweetRepository) {
+	// 	tweetRepository = _tweetRepository;
 		
-		Comparator<Tweet> postedDateSorter = (t1, t2) -> t1.getPosted().compareTo(t2.getPosted());
+	// 	Comparator<Tweet> postedDateSorter = (t1, t2) -> t1.getPosted().compareTo(t2.getPosted());
 		
-		target = _target;
-		before = createBefore(target, new TreeSet<Tweet>(postedDateSorter));
-		after = createAfter(target, new TreeSet<Tweet>(postedDateSorter));
-	}
+	// 	target = _target;
+	// 	before = createBefore(target, new TreeSet<Tweet>(postedDateSorter));
+	// 	after = createAfter(target, new TreeSet<Tweet>(postedDateSorter));
+	// }
 	
-	private Tweet target;
-	
-	private SortedSet<Tweet> before;
-	
-	private SortedSet<Tweet> after;
-	
-	SortedSet<Tweet> createBefore(Tweet _target, SortedSet<Tweet> _before) {
-		while (_target.getInReplyTo() != null) {
-			_target = _target.getInReplyTo();
-			_before.add(_target);
-		}
+	// public ContextDto(Tweet _target) throws NotFoundException {
+	// 	if (tweetRepository == null) {
+	// 		throw new NotFoundException("No tweet repository found.");
+	// 	}
 		
-		return _before;
-	}
+	// 	Comparator<Tweet> postedDateSorter = (t1, t2) -> t1.getPosted().compareTo(t2.getPosted());
+		
+	// 	target = _target;
+	// 	before = createBefore(target, new TreeSet<Tweet>(postedDateSorter));
+	// 	after = createAfter(target, new TreeSet<Tweet>(postedDateSorter));
+	// }
 	
-	SortedSet<Tweet> createAfter(Tweet _target, SortedSet<Tweet> _after) {
-		if (_target == null) {
-			return _after;
-		}
+	// private Tweet target;
+	
+	// private SortedSet<Tweet> before;
+	
+	// private SortedSet<Tweet> after;
+	
+	// SortedSet<Tweet> createBefore(Tweet _target, SortedSet<Tweet> _before) {
+	// 	while (_target.getInReplyTo() != null) {
+	// 		_target = _target.getInReplyTo();
+	// 		_before.add(_target);
+	// 	}
 		
-		Set<Tweet> replies = tweetRepository.findByInReplyTo(_target).get();
+	// 	return _before;
+	// }
+	
+	// SortedSet<Tweet> createAfter(Tweet _target, SortedSet<Tweet> _after) {
+	// 	if (_target == null) {
+	// 		return _after;
+	// 	}
 		
-		if (replies == null || replies.isEmpty()) {
-			return _after;
-		}
+	// 	Set<Tweet> replies = tweetRepository.findByInReplyTo(_target).get();
 		
-		for (Tweet t : replies) {
-			_after.add(t);
-			return createAfter(t, _after);
-		}
-		return createAfter(null, null);
-	}
+	// 	if (replies == null || replies.isEmpty()) {
+	// 		return _after;
+	// 	}
+		
+	// 	for (Tweet t : replies) {
+	// 		_after.add(t);
+	// 		return createAfter(t, _after);
+	// 	}
+	// 	return createAfter(null, null);
+	// }
 }
 
 
