dca5c6a21d0eab598022b74f0d2f3711dab38d59
olivia-e452 <144254461+olivia-e452@users.noreply.github.com>
2023-09-28 16:22:34 -0500

    validatehashtag + validateuser (#89)

--- src/main/java/com/cooksys/spring_assessment/services/impl/ValidateServiceImpl.java
+++ src/main/java/com/cooksys/spring_assessment/services/impl/ValidateServiceImpl.java
@@ -1,6 +1,12 @@
 package com.cooksys.spring_assessment.services.impl;
 
+import java.util.Optional;
+
+import com.cooksys.spring_assessment.entities.User;
+import com.cooksys.spring_assessment.repositories.HashtagRepository;
+import com.cooksys.spring_assessment.repositories.UserRepository;
 import com.cooksys.spring_assessment.services.ValidateService;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -9,4 +15,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ValidateServiceImpl implements ValidateService {
     
+	  private final HashtagRepository hashtagRepository;
+	  private final UserRepository userRepository;
+	  
+	  // checks whether hashtag exists
+	  @Override
+	  public boolean validateHashtag(String label) {
+		  return !hashtagRepository.findByLabel(label).isEmpty();
+	  }
+		
+	  // checks whether username exists (and is not deleted(?))
+	  @Override
+	  public boolean validateUsername(String username) {
+		  Optional<User> user = userRepository.findByCredentialsUsername(username);
+		  return user.isEmpty() ? false : true;
+	  }
 }
